ab7568504c97b3d62bc2c2467e10073ce3a5a614
OLAMILEKAN-AWOPETU <awopetuolamilekanmiliky@gmail.com>
2022-09-08 20:23:24 -0700

    count 4-put .c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  * 
  * Return: Always 0 (successful)
  */
-int main(void)
+int main( void )
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return(0);
